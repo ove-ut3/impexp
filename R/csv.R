7809fd68bfb438a48648600d1cbf2046b142daee
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-09-15 12:50:04 +0200

    Import csv : ajout des paramètres na et warning_type

--- R/csv.R
+++ R/csv.R
@@ -5,18 +5,36 @@
 #' @param fichier Chemin vers le fichier CSV
 #' @param ligne_debut Ligne de début à partir duquel importer.
 #' @param encoding Encodage du fichier CSV.
+#' @param na Caractères à considérer comme vide en plus de \code{c("NA", "", " ")}.
 #' @param col_types Type des champs (utilisé par \code{data.table::fread}.
+#' @param warning_type Affichage des warnings liés aux types des champs importés.
 #'
 #' @return Un data frame.\cr
 #'
 #' @export
-importer_fichier_csv <- function(fichier, ligne_debut = 1, encoding = "Latin-1", col_types = NULL) {
+importer_fichier_csv <- function(fichier, ligne_debut = 1, encoding = "Latin-1", na = NULL, col_types = NULL, warning_type = TRUE) {
 
   if (!file.exists(fichier)) {
     stop("Le fichier \"", fichier,"\" n'existe pas.", call. = FALSE)
   }
 
-  importer_fichier_csv <- data.table::fread(iconv(fichier, from = "UTF-8"), sep = ";", encoding = encoding, na.strings = c("NA", "", " ")) %>%
+  if (warning_type == FALSE) {
+    fonction_import <- purrr::quietly(data.table::fread)
+  } else {
+    fonction_import <- data.table::fread
+  }
+
+  importer_fichier_csv <- fonction_import(iconv(fichier, from = "UTF-8"), sep = ";", encoding = encoding, na.strings = c("NA", "", " ", na))
+
+  if (warning_type == FALSE) {
+    if (any(!stringr::str_detect(importer_fichier_csv$warnings, "Bumped column \\d+? to type"))) {
+      warnings <- importer_fichier_csv$warnings[!stringr::str_detect(importer_fichier_csv$warnings, "Bumped column \\d+? to type")]
+      purrr::walk(warnings, message)
+    }
+    importer_fichier_csv <- importer_fichier_csv[["result"]]
+  }
+
+  importer_fichier_csv <- importer_fichier_csv %>%
     importr::normaliser_nom_champs()
 
   return(importer_fichier_csv)
@@ -30,15 +48,17 @@ importer_fichier_csv <- function(fichier, ligne_debut = 1, encoding = "Latin-1",
 #' @param chemin Chemin du répertoire à partir duquel seront importés les fichiers excel (récursif).
 #' @param ligne_debut Ligne de début à partir duquel importer.
 #' @param encoding Encodage des fichiers CSV.
+#' @param na Caractères à considérer comme vide en plus de \code{c("NA", "", " ")}.
 #' @param col_types Type des champs (utilisé par \code{data.table::fread}).
 #' @param paralleliser \code{TRUE}, import parallelisé des fichiers CSV.
 #' @param archive_zip \code{TRUE}, les fichiers CSV contenus dans des archives zip sont également importés; \code{FALSE} les archives zip sont ignorées.
+#' @param warning_type Affichage des warnings liés aux types des champs importés.
 #' @param message_import \code{TRUE}, affichage du message d'import
 #'
 #' @return Un data frame dont le champ "import" est la liste des data frame importés.
 #'
 #' @export
-importer_masse_csv <- function(regex_fichier, chemin = ".", ligne_debut = 1, encoding = "Latin-1", col_types = NULL, paralleliser = FALSE, archive_zip = FALSE, message_import = TRUE) {
+importer_masse_csv <- function(regex_fichier, chemin = ".", ligne_debut = 1, encoding = "Latin-1", na = NULL, col_types = NULL, paralleliser = FALSE, archive_zip = FALSE, warning_type = FALSE, message_import = TRUE) {
 
   if (!dir.exists(chemin)) {
     stop("Le répertoire \"", chemin,"\" n'existe pas.", call. = FALSE)
@@ -70,7 +90,7 @@ importer_masse_csv <- function(regex_fichier, chemin = ".", ligne_debut = 1, enc
   }
 
   importer_masse_csv <- dplyr::tibble(fichier = unique(fichiers$fichier),
-                                      import = pbapply::pblapply(unique(fichiers$fichier), importer_fichier_csv, ligne_debut = ligne_debut, encoding = encoding, col_types = col_types, cl = cluster))
+                                      import = pbapply::pblapply(unique(fichiers$fichier), importer_fichier_csv, ligne_debut = ligne_debut, encoding = encoding, na = na, col_types = col_types, warning_type = warning_type, cl = cluster))
 
   file.remove(fichiers$fichier) %>%
     invisible()
