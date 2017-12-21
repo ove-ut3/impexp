cd78e7b4b9d8e1acd22fdcc1e2380d19df524181
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-12-21 13:51:40 +0100

    Ajout de la fonction ajouter_lignes_sqlite

--- R/sqlite.R
+++ R/sqlite.R
@@ -58,3 +58,45 @@ exporter_table_sqlite <- function(table, base_sqlite, nom_table = NULL, ecraser 
 
   DBI::dbDisconnect(connexion)
 }
+
+#' Ajouter des lignes a une table SQLite
+#'
+#' Ajouter des lignes à une table SQLite
+#'
+#' @param table Table initiale.
+#' @param table_ajout Table à concaténer.
+#' @param base_sqlite Chemin de la base SQLite.
+#'
+#' @export
+ajouter_lignes_sqlite <- function(table, table_ajout, base_sqlite) {
+
+  if (!file.exists(base_sqlite)) {
+    stop("La base SQLite \"", base_sqlite,"\" n'existe pas...", call. = FALSE)
+  }
+
+  connexion <- DBI::dbConnect(RSQLite::SQLite(), dbname = base_sqlite)
+
+  if (!table %in% DBI::dbListTables(connexion)) {
+    stop("La table \"", table,"\" n'existe pas...", call. = FALSE)
+  }
+
+  table <- DBI::dbReadTable(connexion, table) %>%
+    tibble::as_tibble()
+
+  if (ncol(table) != ncol(table_ajout)) {
+    stop("Le nombre de colonnes de la table initiale et celle à concaténer doit être identique", call. = FALSE)
+  }
+
+  sql <- table_ajout %>%
+    dplyr::mutate(id = row_number()) %>%
+    tidyr::gather("champ", "valeur", -id) %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise(valeur = paste(valeur, collapse = "', '")) %>%
+    dplyr::pull(valeur) %>%
+    paste(collapse = "'), ('") %>%
+    { paste0("INSERT INTO coordonnees VALUES ('", ., "');") }
+
+  DBI::dbExecute(connexion, sql)
+
+  DBI::dbDisconnect(connexion)
+}
